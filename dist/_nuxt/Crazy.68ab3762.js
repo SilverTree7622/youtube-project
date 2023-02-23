520a6c5bffdb52d6590e6835d2d39dddc09aabc8
FluxHunter <40912698+SilverTree7622@users.noreply.github.com>
2023-02-24 00:15:48 +0900

    deploy

---
+++ dist/_nuxt/Crazy.68ab3762.js
@@ -0,0 +1,1 @@
+import{a as e,o as t,m as a}from"./entry.d3262e86.js";const s=e({__name:"Crazy",setup(n){return(o,r)=>(t(),a("div",null," crazy text "))}});export{s as default};
---
+++ dist/_nuxt/app.config.d20ae5b6.js
@@ -0,0 +1,1 @@
+import{B as u,L as s}from"./entry.d3262e86.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
