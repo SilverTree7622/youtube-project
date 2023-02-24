7c701c48adb5dc64f52905967cd8e7122896f33b
FluxHunter <40912698+SilverTree7622@users.noreply.github.com>
2023-02-24 13:20:28 +0900

    style(modal): change close btn position

--- components/Content/Modal.vue
+++ components/Content/Modal.vue
@@ -21,27 +21,34 @@
         >
             <div class="relative w-full h-full max-w-2xl md:h-auto">
                 <!-- Modal content -->
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div class="
+                        relative bg-white rounded-lg shadow
+                        dark:bg-gray-700 flex flex-col
+                    "
+                >
                     <!-- x close btn -->
                     <button
                         class="
                             text-gray-400 bg-transparent
                             hover:bg-gray-200 hover:text-gray-900
                             dark:hover:bg-gray-600 dark:hover:text-white
-                            rounded-lg text-sm p-1.5 ml-auto inline-flex items-center
+                            rounded-lg text-sm p-1.5 ml-auto inline-flex
                         "
                         type="button"
                         @click="onClose"
                         data-modal-hide="staticModal"
                     >
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
                         <span class="sr-only">Close modal</span>
                     </button>
                     <!-- list component -->
                     <component
                         class="
-                            text-xl font-semibold text-gray-900 dark:text-white
-                            p-4
+                            dark:text-white
+                            text-xl font-semibold text-gray-900 p-4
+
                         "
                         :is="context"
                     />
@@ -58,5 +65,4 @@ const props = defineProps<{
     onClose: () => void;
 }>();
 const context = defineAsyncComponent(() => import(`@/components/Content/Bundle/${props.name}.vue`));
-console.log('context: ', context);
 </script>
